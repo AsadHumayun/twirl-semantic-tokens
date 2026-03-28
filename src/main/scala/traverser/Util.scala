7fc0b92db1414d03cba8629688d3dd2aafb4170e
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-03-28 13:10:23 +0000

    feat: clean template output and apply formatting

--- src/main/scala/traverser/Util.scala
+++ src/main/scala/traverser/Util.scala
@@ -18,22 +18,30 @@ object Util {
 	/**Returns the base directory for this project (`user.dir`) */
 	def getBaseDirectory: Path = Path.of(System.getProperty("user.dir"))
 
-// Source - https://stackoverflow.com/a/23129035
-// Posted by serejja, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-03-28, License - CC BY-SA 3.0
-
-	object Implicits {
-		implicit class CaseClassToString(c: AnyRef) {
-			def toStringWithFields: String = {
-				val fields = (Map[String, Any]() /: c.getClass.getDeclaredFields) { (a, f) =>
-					f.setAccessible(true)
-					a + (f.getName -> f.get(c))
-				}
-
-				fields map { case (k, v) =>
-					s"$k=[$v]"
-				} mkString "\n\r"
-			}
+	/**
+	 * This method will iterate over the fields of a case class and present them in a more
+	 * easy-to-digest form. 
+	 * 
+	 * @example {{{
+	 * case class Example(e: String)
+	 * 
+	 * val example = Example("hi!")
+	 * 
+	 * caseClassToString(example) -> "e=[hi!]"
+	 * }}}
+	 * 
+	  * @see {@link https://stackoverflow.com/a/23129035}
+		*		- Posted by serejja, modified by community. See post 'Timeline' for change history
+		*   - Retrieved 2026-03-28, License - CC BY-SA 3.0
+	  */
+	def caseClassToString(c: AnyRef): String = {
+		val fields = (Map[String, Any]() /: c.getClass.getDeclaredFields) { (a, f) =>
+			f.setAccessible(true)
+			a + (f.getName -> f.get(c))
 		}
+
+		fields map { case (k, v) =>
+			s"$k=[$v]"
+		} mkString "\n\r"
 	}
 }

86dc4311de63fe1c2ea8c5387e3adf131b25a94e
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-04-09 17:12:43 +0100

    wip

--- src/main/scala/traverser/Traverser.scala
+++ src/main/scala/traverser/Traverser.scala
@@ -47,51 +47,173 @@ class Traverser {
     Source.fromFile(new File(path)).mkString("")
   }
 
-  def matchTree(tree: TemplateTree) = {
-    /** A case class designed to make it easier to make it easier to construct and convert semantic
-      * tokens to/from their raw representation that is expected by LSP.
-      *
-      * @param deltaLine
-      *   token line number, relative to the start of the previous token
-      * @param deltaStart
-      *   token start character, relative to the start of the previous token (relative to `0` or the
-      *   previous token’s start if they are on the same line)
-      * @param length
-      *   length of the token
-      * @param tokenType
-      *   will be looked up in `SemanticTokensLegend.tokenTypes`. We currently ask that `tokenType`
-      *   < `65536`.
-      * @param tokenModifiers
-      *   each set bit will be looked up in `SemanticTokensLegend.tokenModifiers`
-      */
-    case class TwirlSemanticToken(
-      val deltaLine     : Int,
-      val deltaStart    : Int,
-      val length        : Int,
-      val tokenType     : Int,
-      val tokenModifiers: Int,
-    ) {
-      def toList: List[Int] = List(deltaLine, deltaStart, length, tokenType, tokenModifiers)
-    }
+  /** The idea is that this class will hold the state for the recursive logic when it comes to
+    * "jumping" between the ASTs.
+    *
+    * @param prevPos
+    *   The previous `Position`
+    * @param tokens
+    *   All of the collected semantic tokens thus far
+    */
+  case class State(
+    val prevPos: Position,
+    val tokens: Seq[TwirlSemanticToken],
+  )
 
-    def getLength(token: Comment | Display | Plain): Int = {
-      token match {
-        case Comment(msg)  => msg.length
-        case Display(exp)  => exp.parts.length
-        case Plain(text)   => text.length
-      }
-    }
-
+  /** A case class designed to make it easier to make it easier to construct and convert semantic
+    * tokens to/from their raw representation that is expected by LSP.
+    *
+    * @param deltaLine
+    *   token line number, relative to the start of the previous token
+    * @param deltaStart
+    *   token start character, relative to the start of the previous token (relative to `0` or the
+    *   previous token’s start if they are on the same line)
+    * @param length
+    *   length of the token
+    * @param tokenType
+    *   will be looked up in `SemanticTokensLegend.tokenTypes`. We currently ask that `tokenType` <
+    *   `65536`.
+    * @param tokenModifiers
+    *   each set bit will be looked up in `SemanticTokensLegend.tokenModifiers`
+    */
+  case class TwirlSemanticToken(
+    val deltaLine: Int,
+    val deltaStart: Int,
+    val length: Int,
+    val tokenType: Int,
+    val tokenModifiers: Int,
+  ) {
+    def toList: List[Int] = List(deltaLine, deltaStart, length, tokenType, tokenModifiers)
+    def toPos: Position   = Position(deltaLine, deltaStart)
   }
 
-  def provideSemanticTokens(trees: scala.collection.Seq[TemplateTree]) = {
-    // Not too sure as to what we should do here - maybe just iterate over the tree?
-    // Not sure how I should be sending these back to the LS.
+  /** This class stores delta information for a token. This contains the raw position of these
+    * tokens and not the relative delta tokens that are expected by the LSP (i.e. VSCode)
+    *
+    * @param line
+    *   The line number of the token
+    * @param column
+    *   The column number of the token
+    */
+  case class Position(
+    val line: Int,
+    val column: Int,
+  )
 
-    trees map {
-      tree => this.matchTree(tree)
+  def traverseReassignment()
+
+  def matchNode(node: TemplateTree, prevToken: TwirlSemanticToken): TwirlSemanticToken = {
+    def getDeltaPos(curr: Position, prev: Position): Position =
+      Position(
+        line = curr.line - prev.line,
+        column = curr.column - prev.column,
+      )
+
+    node match {
+      case comment @ Comment(msg)       =>
+        /** I tried to get this into its own function or suchlike, but I was unable to, since in
+          * order to extract the `pos`, I need to extract the types using the pattern matching
+          * extractors.
+          *
+          * Maybe I could do something like this by just manually supplying the `pos` attrs as a
+          * property in the function, but I have not tried this yet in the spike here. Actually now
+          * that I think abuot it, if I manually make things that rely on `pos` and then have them
+          * passed to a function, then it's only really going to have one line in the resulting
+          * function which I'm not sure about the value of having a function for something.... that
+          * small. (i.e. just making a case class)
+          *
+          * The @ symbol makes it so that the whole object/class is extracted from the pattern
+          * match, as opposed to just the string (since we need to get access to its other
+          * attrs/props).
+          *
+          * Also not sure as to how I'm going to be able to map over the list and keep doing it
+          * "over" the previous item if you get me. I'm sure that something like `fold` or something
+          * will be able to accomodate this.
+          */
+        val delta = getDeltaPos(
+          curr = Position(comment.pos.line, comment.pos.column),
+          prev = prevToken.toPos,
+        )
+
+        TwirlSemanticToken(
+          deltaLine = delta.line,
+          deltaStart = delta.column,
+          length = msg.length,
+          // TODO: Actually understand what `tokenType` and `tokenModifiers`
+          // are expected to be by LSP and what they should be/represent.
+          tokenType = 1,
+          tokenModifiers = 111,
+        )
+      case display @ Display(exp)       =>
+        // Scala source
+        val delta = getDeltaPos(
+          curr = Position(display.pos.line, display.pos.column),
+          prev = prevToken.toPos,
+        )
+
+        TwirlSemanticToken(
+          deltaLine = delta.line,
+          deltaStart = delta.column,
+          length = exp.parts.length,
+          // ======================
+          // Still need to actually figure out and fix this part
+          // ======================
+          tokenType = 1,
+          tokenModifiers = 111,
+        )
+      case plain @ Plain(text)          =>
+        val delta = getDeltaPos(
+          curr = Position(plain.pos.line, plain.pos.column),
+          prev = prevToken.toPos,
+        )
+
+        TwirlSemanticToken(
+          deltaLine = delta.line,
+          deltaStart = delta.column,
+          length = text.length,
+          // ======================
+          // Still need to actually figure out and fix this part
+          // ======================
+          tokenType = 1,
+          tokenModifiers = 111,
+        )
+      case reassign @ Reassignment(ref) =>
+        /** I don't really know how to go about recuring like this... */
+        // New plan: make a different function to traverse the Reassignment side,
+        // and just get it to pass around state
+        ???
+      case simpleCode @ Simple(code)    =>
+        // Triggered by ScapaExpPart...
+        val delta = getDeltaPos(
+          curr = Position(simpleCode.pos.line, simpleCode.pos.column),
+          prev = prevToken.toPos,
+        )
+
+        TwirlSemanticToken(
+          deltaLine = delta.line,
+          deltaStart = delta.column,
+          length = code.length,
+          // ======================
+          // Still need to actually figure out and fix this part
+          // ======================
+          tokenType = 1,
+          tokenModifiers = 111,
+        )
+
+      // case blockCode  @ Block(whitespace, args, contents)        =>
+
+      case scalaExp @ ScalaExp(parts) =>
+        parts.map()
+      case _: TemplateTree            => ???
     }
   }
+
+  /** This function should loop over the nodes and then send them back.
+    *
+    * Use foldLeft for the folding
+    */
+  def provideSemanticTokens(nodes: scala.collection.Seq[TemplateTree]) =
+    nodes
 
   def getTwirlTemplateSemanticTokens(params: SemanticTokensParams) = {
     import play.twirl.parser.{ TreeNodes, TwirlParser }

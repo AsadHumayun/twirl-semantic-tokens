3da76406f9c3af5b37324ec015d00a69ea775f10
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-04-10 08:45:11 +0100

    feat: SemanticTokensService

---
+++ src/main/scala/traverser/SemanticTokensService.scala
@@ -0,0 +1,74 @@
+package traverser
+
+/** This case object is 'infrastructure' that is likely already present somewhere in their codebase,
+  * so this is really to try and assert that things are working locally, and this is not what things
+  * are likely to look like.
+  *
+  * This will likely be something that is either addressed in the PR to merge this into Metals
+  * (should things go well...) but yeah this will require discussion with some reviewers/maintainers
+  * as I just don't know enough about the Metals codebase to properly assert something like this.
+  */
+case object SemanticTokensService {
+  case object types {
+    private val typesList = List(
+      "type",
+      "class",
+      "enum",
+      "interface",
+      "struct",
+      "typeParameter",
+      "parameter",
+      "variable",
+      "property",
+      "enumMember",
+      "event",
+      "function",
+      "method",
+      "macro",
+      "keyword",
+      "modifier",
+      "comment",
+      "string",
+      "number",
+      "regexp",
+      "operator",
+      "decorator",
+      "label",
+    )
+
+    private val typesMap: Map[String, Int] = typesList.zipWithIndex.toMap
+
+    def resolve(searchTerm: String): Int =
+      typesMap.get(searchTerm) match {
+        case Some(value) => value
+        /**
+          * ========================
+          * This could cause issues later down the line - how do I want to
+          * go about handling errors like these?
+          * ========================
+          */
+        case None        => 0
+      }
+  }
+
+  case object modifiers {
+    private val modifiersList = List(
+      "declaration",
+      "definition",
+      "readonly",
+      "static",
+      "deprecated",
+      "abstract",
+      "async",
+      "modification",
+      "documentation",
+      "defaultLibrary",
+    )
+    private val modifiersMap: Map[String, Int] = modifiersList.zipWithIndex.toMap
+    def resolve(searchTerm: String): Int =
+      modifiersMap.get(searchTerm) match {
+        case Some(value) => value
+        case None        => 0
+      }
+  }
+}
